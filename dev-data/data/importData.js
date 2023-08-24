d31e507b8b0ccc15147dcd1e98523a2a118e6875
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-08-24 22:08:46 +0100

    Import Data

--- dev-data/data/importData.js
+++ dev-data/data/importData.js
@@ -1,4 +1,28 @@
 const fs = require('fs');
 const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const Course = require('../../models/courseModel');
 const User = require('../../models/userModel');
+
+dotenv.config({ path: './config.env' });
+
+//CONNECTING MONGOOSE
+//REPLACING PASSWORD IN CONNECTION STRING
+const DB = process.env.DATABASE.replace(
+  '<password>',
+  process.env.DATABASE_PASSWORD
+);
+
+//CONNECTING DATABASE
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log('DB connection successful'));
+
+// READ FILE
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/courses.json`, 'utf-8')
+);
