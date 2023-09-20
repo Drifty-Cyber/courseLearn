466e3f9e99c75222bc298ab0df497ff2dec744fe
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-20 20:51:32 +0100

    External Error Handling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const xss = require('xss-clean');
 const coursesRouter = require('./routes/courseRoutes');
 const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 
 // Create Express app
 const app = express();
@@ -33,15 +34,16 @@ app.all('*', (req, res, next) => {
 });
 
 // Error Handler
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
+// app.use((err, req, res, next) => {
+//   err.statusCode = err.statusCode || 500;
+//   err.status = err.status || 'error';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+//   res.status(err.statusCode).json({
+//     status: err.status,
+//     message: err.message,
+//   });
+// });
+app.use(globalErrorHandler);
 
 // Export App
 module.exports = app;
