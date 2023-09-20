cf8afbdae66f6e8c95467ad8a5d94bf64d6ed78a
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-20 22:51:24 +0100

    Error Handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,6 +1,6 @@
 const AppError = require('../utils/appError');
 
-const handleDuplicateFields = (err) => {
+const handleDuplicateFieldsDB = (err) => {
   const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
   const message = `Duplicate field ${value}. Please use another value!`;
 
@@ -45,18 +45,21 @@ module.exports = (err, req, res, next) => {
   err.status = err.status || 'error';
 
   if (process.env.NODE_ENV === 'development') {
+    // console.log(err);
     sendErrorDev(err, req, res);
   } else if (process.env.NODE_ENV === 'production') {
     let error = { ...err };
     error.message = err.message;
 
-    if (err.code === 11000) error = handleDuplicateFields(err);
     if (err.name === 'CastError') error = handleCastErrorDB(err);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+
+    sendErrorProd(error, req, res);
   }
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    error: err,
-  });
+  // res.status(err.statusCode).json({
+  //   status: err.status,
+  //   message: err.message,
+  //   error: err,
+  // });
 };
