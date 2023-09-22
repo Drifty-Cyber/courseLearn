7ccf38da62b9c9340c9bf7e3de2ebdd532b31ab8
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-22 22:04:26 +0100

    Error Handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,17 +1,26 @@
 const AppError = require('../utils/appError');
 
+// Handle Duplicate Fields
 const handleDuplicateFieldsDB = (err) => {
   const value = err.message.match(/(["'])(\\?.)*?\1/)[0];
   const message = `Duplicate field ${value}. Please use another value!`;
-
   return new AppError(message, 400);
 };
 
+// Handle Cast Error
 const handleCastErrorDB = (err) => {
   const message = `Invalid ${err.path}: ${err.value}`;
   return new AppError(message, 400);
 };
 
+// Handle Validation Error
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((errObj) => errObj.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+// Send Error in 'Development' Environment
 const sendErrorDev = (err, req, res) => {
   if (req.originalUrl.startsWith('/api')) {
     return res.status(err.statusCode).json({
@@ -25,6 +34,7 @@ const sendErrorDev = (err, req, res) => {
   // If Rendered Website becomes available⬇⬇⬇⬇⬇
 };
 
+// Send Error in 'Production' Environment
 const sendErrorProd = (err, req, res, next) => {
   if (err.isOperational) {
     return res.status(err.statusCode).json({
@@ -40,6 +50,7 @@ const sendErrorProd = (err, req, res, next) => {
   });
 };
 
+// Export Error Function
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
@@ -52,6 +63,7 @@ module.exports = (err, req, res, next) => {
 
     if (err.name === 'CastError') error = handleCastErrorDB(err);
     if (err.code === 11000) error = handleDuplicateFieldsDB(err);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
 
     sendErrorProd(error, req, res);
   }
