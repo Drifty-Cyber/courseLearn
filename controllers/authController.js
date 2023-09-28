8f5568059155aecdbf5244d9bee96d0a6875bee4
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-28 16:43:46 +0100

    JWT

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,11 @@
+const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
+
+// const signToken = (id) => {
+//   jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: })
+// }
 
 // Signup Controller
 exports.signup = catchAsync(async (req, res, _next) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -3,11 +3,11 @@ const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
 // Utility Function
-const sendResponse = (statusCode, payload, req, res) => {
+const sendResponse = (statusCode, data, req, res) => {
   res.status(statusCode).json({
     status: 'success',
     data: {
-      payload,
+      data,
     },
   });
 };
@@ -18,24 +18,26 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
   const users = await User.find();
 
   // Response
-  res.status(200).json({
-    status: 'success',
-    data: {
-      users,
-    },
-  });
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       users,
+  //     },
+  //   });
+  sendResponse(200, users, req, res);
 });
 
 // Get user by ID
 exports.getUser = catchAsync(async (req, res, next) => {
   // Query
-  const user = await User.findById(req.params.id);
+  const user = await User.findById(req.params.id).select('+active');
 
   // Response
-  res.status(200).json({
-    status: 'success',
-    data: {
-      user,
-    },
-  });
+  //   res.status(200).json({
+  //     status: 'success',
+  //     data: {
+  //       user,
+  //     },
+  //   });
+  sendResponse(200, user, req, res);
 });
