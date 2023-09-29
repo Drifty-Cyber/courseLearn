ff3d3f56f543a9723d684f45b71e1a7c92091ada
Drifty-Cyber <raphaelfadimu@gmail.com>
2023-09-29 19:10:01 +0100

    Signup Func.

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,38 +3,37 @@ const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
 
-const signToken = (id) => {
-  return jwt.sign({ id: id }, process.env.JWT_SECRET, {
+const signToken = (id) =>
+  jwt.sign({ id: id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
+
+const createSendToken = (user, statusCode, req, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    // secure:
+    //   req.secure === 'true' || req.headers['x-forwarded-proto'] === 'true',
+  };
+
+  res.cookie('jwt', token, cookieOptions);
+
+  // Remove password from output
+  user.password = undefined;
+
+  // Response
+  res.status(statusCode).json({
+    status: 'success',
+    token,
+    data: {
+      user,
+    },
+  });
 };
-
-// const createSendToken = (user, statusCode, req, res) => {
-//   const token = signToken(user._id);
-
-//   const cookieOptions = {
-//     expires: new Date(
-//       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-//     ),
-//     httpOnly: true,
-//     // secure:
-//     //   req.secure === 'true' || req.headers['x-forwarded-proto'] === 'true',
-//   };
-
-//   res.cookie('jwt', token, cookieOptions);
-
-//   // Remove password from output
-//   user.password = undefined;
-
-//   // Response
-//   res.status(statusCode).json({
-//     status: 'success',
-//     token,
-//     data: {
-//       user,
-//     },
-//   });
-// };
 
 // Signup Controller
 exports.signup = catchAsync(async (req, res, _next) => {
@@ -45,18 +44,7 @@ exports.signup = catchAsync(async (req, res, _next) => {
     passwordConfirm: req.body.passwordConfirm,
   });
 
-  // const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-  //   expiresIn: process.env.JWT_EXPIRES_IN,
-  // });
-  const token = signToken(newUser._id);
-
-  res.status(201).json({
-    status: 'success',
-    token,
-    newUser,
-  });
-
-  // createSendToken(newUser, 201, req, res);
+  createSendToken(newUser, 201, req, res);
 });
 
 // Login
